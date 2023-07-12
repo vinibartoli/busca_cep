7b0398b74ca08aefabd5ac587cc6f2357674671c
vinibartoli <102329694+vinibartoli@users.noreply.github.com>
2023-07-12 19:24:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# buscacep
+Busca CEP - site para realizar buscas de informações CEP
+
+Objetivo principal:
+
+Projeto criado para aprendizado de consumo de API (API utilizada: ViaCEP) e funções assincronas (async e await)
+
+## Stack utilizada
+
+HTML, CSS e JAVA SCRIPT
+
+## 🚀 Sobre mim
+Sou o Vinicius, atualmente tenho 22 anos, graduando em Analise e Desenvolvimento de sistemas e buscando aprender e aprimorar cada vez mais minhas habilidades no mundo da pragramação
---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+let cep = document.querySelector('#input-cep')
+let localidade = document.querySelector("#input-localidade")
+let uf = document.querySelector("#input-uf")
+let logradouro = document.querySelector("#input-logradouro")
+let bairro = document.querySelector("#input-bairro")
+let ddd = document.querySelector("#input-ddd")
+let regexNumeros = /^\d+$/
+
+const btnSubmit = document.querySelector(".submit-cep").addEventListener('click', getCep)
+const btnClear = document.querySelector(".clear").addEventListener('click', clearInputs)
+
+
+async function getCep() {
+  const cepValue = cep.value
+  const url = `https://viacep.com.br/ws/${cepValue}/json/`
+  const resultRegex = regexNumeros.test(cepValue)
+
+  if(cepValue.length < 8 || resultRegex == false) {
+    alert("O CEP deve conter 8 digitos, sendo eles apenas números")
+    cep.value = ""
+  } else {
+    const dados = await fetch(url)
+    const endereco = await dados.json() 
+
+    if(endereco.erro == true) {
+      alert("Digite um CEP válido")
+
+      localidade.value = ""
+      uf.value = ""
+      logradouro.value = ""
+      bairro.value = ""
+      ddd.value = ""
+      cep.value = ""
+    } else {
+      localidade.value = endereco.localidade
+      uf.value = endereco.uf
+      logradouro.value = endereco.logradouro
+      bairro.value = endereco.bairro
+      ddd.value = endereco.ddd
+    }
+  }
+}
+
+function clearInputs() {
+  localidade.value = ""
+  uf.value = ""
+  logradouro.value = ""
+  bairro.value = ""
+  ddd.value = ""
+  cep.value = ""
+}
